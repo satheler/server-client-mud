874dd48f2ce918baa9164d506bcce9c6c5d4b96d
Sabrina <sabrinacarlew@gmail.com>
2017-11-11 00:56:48 -0200

    Serialização e Deserialização

---
+++ MUD/MUD/MUD.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import json  #Outra opção é usar o import cPickle as pickle, que possui os mesmos métodos para JSON listados abaixo
+#dump() serializa arquivos abertos, dumps({dados}, sort_keys = true, indent = value, separatos = ',', ':') serializa caracteres, load(open()) deserializa objetos do tipo file, loads() deserializa caeacteres
+class CustomEncoder(json.JSONEncoder): #Precisa ser implementado para grafos complexos(quando temos mais de uma classe), separa em chaves cada tipo de dado que queremos serializar, exemplo do site:
+
+    def default(self, o): #Método padrão a ser implementado
+
+         if isinstance(o, datetime): #retorna se a data for uma instância de datatime / comparações para separar o retorno de cada classe
+ 
+             return {'__datetime__': o.replace(microsecond=0).isoformat()} 
+ 
+         return {'__{}__'.format(o.__class__.__name__): o.__dict__} #retorna os dados de outra classe
